debe64fb866ed3f451098829d3efa915362d36b9
Chris-Effah <akboateng41@gmail.com>
2023-10-04 10:51:02 -0700

    fixed a trailing whitespace error

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,20 +1,21 @@
 #include "binary_trees.h"
+
 /**
- * binary_tree_height - a function that measures the height of a binary tree
- *  @tree: tree to measure the height
- *  Return: height of tree
- **/
+ * binary_tree_height - Measure the height of a binary tree.
+ * @tree: A pointer to the root node of the tree.
+ *
+ * Return: The height of the tree. 0 if the tree is NULL.
+ */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height;
-	size_t right_height;
+	size_t height_left, height_right;
 
 	if (tree == NULL)
-	{
 		return (0);
-	}
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-	return ((left_height > right_height) ?
-		(left_height + 1) : (right_height + 1));
+
+	height_left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	height_right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+
+	return (height_left > height_right ? height_left : height_right);
 }
+
