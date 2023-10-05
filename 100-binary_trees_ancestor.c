c6977bd3131aca7e6d608d79cca14f8f75c247bc
Akos-1 <veronicagyasi@gmail.com>
2023-10-05 14:59:44 -0700

    updated

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -6,7 +6,8 @@
  * @second: Pointer to the second node
  * Return: Pointer to the lowest common ancestor node, or NULL if not found
  **/
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+	       	const binary_tree_t *second)
 {
 	if (first == NULL || second == NULL)
 	{
