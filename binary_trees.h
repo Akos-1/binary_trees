1435c75eca1c3065eb04d98ab64a508f51b8a4f0
Chris-Effah <akboateng41@gmail.com>
2023-10-05 21:43:14 -0700

    betty checks

--- binary_trees.h
+++ binary_trees.h
@@ -17,10 +17,10 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
@@ -36,8 +36,6 @@ typedef struct binary_tree_s heap_t;
 
 
 /*** Prototypes ****/
-/*static int print_t(const binary_tree_t *tree, int offset, int depth, char **s);*/
-/*static size_t _height(const binary_tree_t *tree);*/
 void binary_tree_print(const binary_tree_t *tree);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
@@ -59,7 +57,8 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+		const binary_tree_t *second);
 
 #endif
 
