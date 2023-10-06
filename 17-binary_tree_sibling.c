b487abee4e4410edd21aaccdb1c2c66ebc77666e
Chris-Effah <akboateng41@gmail.com>
2023-10-05 21:55:01 -0700

    betty checks

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -12,17 +12,9 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 		return (NULL);
 	}
 
-	binary_tree_t *parent = node->parent;
+	if (node->parent->left == node)
+		return (node->parent->right);
+	return (node->parent->left);
 
-	if (parent->right != NULL && parent->left == node)
-	{
-		return (parent->right);
-	}
 
-	if (parent->left != NULL && parent->right == node)
-	{
-		return (parent->left);
-	}
-
-	return (NULL);
 }
